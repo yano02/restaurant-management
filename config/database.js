5832a31ccac1a89ec34deebbd4fe7227f692f77e
yannick Namboure <yanoboston@gmail.com>
2025-04-21 12:01:33 +0000

    text

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 // The connexion to the database
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('restaurantdb', 'postgres', 'samwin25', {
+const sequelize = new Sequelize('restaurantdb', 'postgres', 'yano', {
   host: 'localhost',
   dialect: 'postgres',
-  port: 5432,
+  port: 5433,
   logging: false // quiet mode
 });
 
